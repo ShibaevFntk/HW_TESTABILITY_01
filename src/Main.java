dea2f9f0644eb8fad7f958e79cbe30d44d270572
ShibaevFntk <shibaevfntk@gmail.com>
2025-05-20 10:52:06 +0300

    Comments update

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticketCost = 13686; //цена билета
-        int bonusStep = 20; //за сколько начисляем милю
-        int bonus = ticketCost / bonusStep; //количество бонусов (сразу посчитаем)
+        int ticketCost = 13686; //для хранения стоимости билета
+        int bonusStep = 20; //для хранения количества рублей для одной бонусной мили
+        int bonus = ticketCost / bonusStep; //рассчитываю количество бонусных миль, используя
+        // значения заведённых переменных, ответ сохраняю в новую переменную
 
         // Выводим информацию для клиента
         System.out.println("Добрый день, уважаемый клиент!");
         System.out.println("Вами куплен билет за " + ticketCost + " руб.");
-        System.out.println("Вам начислено " + bonus + " б.м.");
+        System.out.println("Вам начислено " + bonus + " б.м."); // вывожу на экран
         System.out.println("Согласно правилам акции, мили начисляеются за каждые " + bonusStep + " руб.");
-        System.out.println("Не прошли по акции " + (ticketCost - bonus * bonusStep) + " руб.")
+        System.out.println("Не прошли по акции " + (ticketCost - bonus * bonusStep) + " руб.");
+
     }
 }
