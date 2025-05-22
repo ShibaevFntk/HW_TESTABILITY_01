7c384faf2997820a89cc09852d1e8fee64d20fcb
ShibaevFntk <ShibaevFntk@gmail.com>
2025-05-22 14:32:27 +0300

    HW correct

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,8 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
-        int miles = service.calculate(price); // должно получиться 500
-        //System.out.println(miles);
-        System.out.println("Добрый день, уважаемый клиент!");
-        System.out.println("Вами куплен билет за " + price + " руб.");
-        System.out.println("Вам начислено " + miles + " б.м."); // вывожу на экран
-        //System.out.println("Согласно правилам акции, мили начисляются за каждые " + bonusStep + " руб.");
-        //System.out.println("Не прошли по акции " + (price - bonus * bonusStep) + " руб.");
+        int miles = service.calculate(price);
+        System.out.println(miles);
 
     }
 }
