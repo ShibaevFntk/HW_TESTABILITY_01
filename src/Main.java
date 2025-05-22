3393dba9e24264a5f759bc35dd672ef9fb72bb68
ShibaevFntk <ShibaevFntk@gmail.com>
2025-05-22 14:33:43 +0300

    HW correct

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         int ticketCost = 13686; //для хранения стоимости билета
-        int bonusStep = 20; //для хранения количества рублей для одной бонусной мили
-        int bonus = ticketCost / bonusStep; //рассчитываю количество бонусных миль, используя
+        int bonus = ticketCost / 20; //рассчитываю количество бонусных миль, используя
         // значения заведённых переменных, ответ сохраняю в новую переменную
 
-        // Выводим информацию для клиента
-        System.out.println("Добрый день, уважаемый клиент!");
-        System.out.println("Вами куплен билет за " + ticketCost + " руб.");
-        System.out.println("Вам начислено " + bonus + " б.м."); // вывожу на экран
-        System.out.println("Согласно правилам акции, мили начисляеются за каждые " + bonusStep + " руб.");
-        System.out.println("Не прошли по акции " + (ticketCost - bonus * bonusStep) + " руб.");
-
+        System.out.println(bonus); // вывожу на экран
     }
 }
